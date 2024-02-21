02f6ca9045166b69ea00e911f782eab4138c8afc
Dima-McArrow <dsm.box@gmail.com>
2024-02-21 17:35:02 +0100

    minor corrections

--- CMS/base_config/create_admin.php
+++ CMS/base_config/create_admin.php
@@ -1,25 +1,25 @@
 <?php
 require_once '../app_config/db_config.php';
 
-// Check if admin user already exists
+// Vérifier si l'utilisateur administrateur existe déjà
 $query = "SELECT * FROM users WHERE email = :email";
 $stmt = $pdo->prepare($query);
-$stmt->execute(['email' => 'vincent.parrot@mail.com']); // Change email as needed
+$stmt->execute(['email' => 'admin@admin.com']);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// If admin user doesn't exist, create one
+// Si l'utilisateur admin n'existe pas, le créer
 if (!$user) {
-    $name = 'default admin';
-    $email = 'admin@admin.com';
-    $password = password_hash('admin', PASSWORD_DEFAULT);
+  $name = 'default admin';
+  $email = 'admin@admin.com';
+  $password = password_hash('admin', PASSWORD_DEFAULT);
 
-    // Insert admin user into the database
-    $query = "INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, 'admin')";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+  // Insérer l'utilisateur admin dans la base de données
+  $query = "INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, 'admin')";
+  $stmt = $pdo->prepare($query);
+  $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
 
-    echo 'Admin user created successfully.';
+  echo 'Admin user created successfully.';
 } else {
-    echo 'Admin user already exists.';
+  echo 'Admin user already exists.';
 }
 ?>
